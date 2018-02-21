37c4a2fc582a3869d173dde686cc5e1e47d40162
Jay Patel <imjaypatel12@gmail.com>
2018-02-21 02:04:38 -0800

    Updated User model Added missing columns Added 2 constructors

--- src/main/java/techit/model/User.java
+++ src/main/java/techit/model/User.java
@@ -15,69 +15,237 @@ import com.fasterxml.jackson.annotation.JsonProperty.Access;
 @Table(name = "users")
 public class User implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    @Id
-    @GeneratedValue
-    private Long id;
+	@Id
+	@GeneratedValue
+	private Long id;
 
-    @Column(nullable = false, unique = true)
-    private String username;
+	@Column(nullable = false, unique = true)
+	private String username;
 
-    @JsonProperty(access = Access.WRITE_ONLY)
-    @Column(nullable = false)
-    private String password;
+	@JsonProperty(access = Access.WRITE_ONLY)
+	@Column(nullable = false)
+	private String password;
 
-    private boolean enabled = true;
+	@Column(nullable = false)
+	private boolean enabled = true;
 
-    public User()
-    {
-    }
+	@Column(nullable = false)
+	private String firstName;
 
-    public Long getId()
-    {
-        return id;
-    }
+	@Column(nullable = false)
+	private String lastName;
+	
+	@Column()
+	private String phoneNumber;
+	
+	@Column()
+	private String department;
+	
+	@Column()
+	private String email;
+	
+	@Column(nullable = false)
+	private Position status;
+	
+	@Column(nullable = false)
+	private int unitId;
 
-    public void setId( Long id )
-    {
-        this.id = id;
-    }
+	// Types of users on the system.
+	private enum Position {
+		SYS_ADMIN(0), SUPERVISING_TECHNICIAN(1), TECHNICIAN(2), USER(3);
 
-    public String getUsername()
-    {
-        return username;
-    }
+		private int positionValue;
 
-    public void setUsername( String username )
-    {
-        this.username = username;
-    }
+		Position(int position_value) {
+			this.positionValue = position_value;
+		}
 
-    public String getPassword()
-    {
-        return password;
-    }
+		public int getValue() {
+			return positionValue;
+		}
+	};
 
-    public void setPassword( String password )
-    {
-        this.password = password;
-    }
+	public User() {
+	}
 
-    public boolean isEnabled()
-    {
-        return enabled;
-    }
+	// Simple constructor for regular users ( students )
+	public User(long id, String firstname, String lastname, String username, String password) {
+		this.id = id;
+		this.firstName = firstname;
+		this.lastName = lastname;
+		this.username = username;
+		this.status = Position.USER;
+		this.unitId = 0; // User does not belongs to any unit\
+		this.password = password;
+		this.enabled = true;
+	}
 
-    public void setEnabled( boolean enabled )
-    {
-        this.enabled = enabled;
-    }
+	// Full user paramenter constructor
+	public User(long id, String firstname, String lastname, String username, String password, String phone,
+			String email, String department, int position, int unit_id) {
+		this.id = id;
+		this.firstName = firstname;
+		this.lastName = lastname;
+		this.username = username;
+		this.phoneNumber = phone;
+		this.email = email;
+		this.department = department;
+		switch (position) {
+		case 0:
+			this.status = Position.SYS_ADMIN;
+			break;
+		case 1:
+			this.status = Position.SUPERVISING_TECHNICIAN;
+			break;
+		case 2:
+			this.status = Position.TECHNICIAN;
+			break;
+		case 3: // These assume that you want to enter a new technician
+		default: // and somehow enter a number thats not intended
+			this.status = Position.USER;
+			break;
+		}
 
-    @Override
-    public String toString()
-    {
-        return "[" + id + ", " + username + ", " + password + "]";
-    }
+		this.unitId = unit_id;
+
+		this.password = password;
+		this.enabled = true;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public String getEmailFromUsername() {
+		return username + "@calstatela.edu"; // Since username has the same
+												// header as email with the
+												// "@calstatela.edu" domain
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getStatus() {
+		return status.getValue();
+	}
+
+	public void setStatus(int position) {
+		switch (position) {
+		case 0:
+			this.status = Position.SYS_ADMIN;
+			break;
+		case 1:
+			this.status = Position.SUPERVISING_TECHNICIAN;
+			break;
+		case 2:
+			this.status = Position.TECHNICIAN;
+			break;
+		case 3:
+			this.status = Position.USER;
+			break;
+		}
+	}
+
+	public String getStatusString() {
+		String status = "";
+		switch (getStatus()) {
+		case 0:
+			status = "SYSTEM ADMINISTRATOR";
+			break;
+		case 1:
+			status = "SUPERVISING TECHNICIAN";
+			break;
+		case 2:
+			status = "TECHNICIAN";
+			break;
+		case 3:
+		default:
+			status = "USER";
+			break;
+		}
+
+		return status;
+	}
+
+	public int getUnitId() {
+		return unitId;
+	}
+
+	public void setUnitId(int unitId) {
+		this.unitId = unitId;
+	}
+
+	@Override
+	public String toString() {
+		return "[" + id + ", " + username + ", " + password + ", " + firstName + ", " + lastName + ", " + phoneNumber
+				+ ", " + email + ", " + department + ", " + status + ", " + unitId + "]";
+	}
 
 }
