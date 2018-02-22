a7a953c9d3693bd715358269610b4c70ea938ec3
Jay Patel <imjaypatel12@gmail.com>
2018-02-21 16:48:30 -0800

    Added Model for Unit

---
+++ src/main/java/techit/model/Unit.java
@@ -0,0 +1,98 @@
+package techit.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "units")
+public class Unit implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue
+	private int id; // Unit's unique id.
+	
+	@Column(nullable = false)
+	private String name; // Name of the department.
+
+	// A unit may have more than one supervisor. This will allow them to assign
+	// temporary leads when they are gone.
+	@Column(nullable = false)
+	private String phone;
+	
+	@Column(nullable = false)
+	private String location;
+	
+	@Column(nullable = false)
+	private String email;
+
+	@Column
+	private String description;
+
+	public Unit() {
+		
+	}
+
+	public Unit(String name, String phone, String location, String email, String description) {
+		super();
+		this.name = name;
+		this.phone = phone;
+		this.location = location;
+		this.email = email;
+		this.description = description;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
