68d453c45ecc1588b73b60880f1bf99d8052b2c8
Jay Patel <jam12928@gmail.com>
2018-02-23 18:23:41 -0800

    Added DAO for Ticket

---
+++ src/main/java/techit/model/dao/TicketDao.java
@@ -0,0 +1,20 @@
+package techit.model.dao;
+
+import java.util.List;
+
+import techit.model.Ticket;
+import techit.model.Unit;
+import techit.model.User;
+
+public interface TicketDao {
+
+	Ticket getTicket(Long id);
+
+	List<Ticket> getTickets();
+
+	Ticket saveTicket(Ticket ticket);
+	
+	List<Ticket> getTickets(Unit unit);
+	
+	List<Ticket> getTickets(User technician);
+}
