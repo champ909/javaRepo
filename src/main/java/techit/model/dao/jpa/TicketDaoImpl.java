10134b8b7b20f8edfd634540a547b8043f7547b4
Jay Patel <jam12928@gmail.com>
2018-02-23 20:31:11 -0800

    Added TicketDaoImpl

---
+++ src/main/java/techit/model/dao/jpa/TicketDaoImpl.java
@@ -0,0 +1,51 @@
+package techit.model.dao.jpa;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import techit.model.Ticket;
+import techit.model.Unit;
+import techit.model.User;
+import techit.model.dao.TicketDao;
+
+public class TicketDaoImpl implements TicketDao {
+
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	@Override
+	public Ticket getTicket(Long id) {
+		return entityManager.find(Ticket.class, id);
+	}
+
+	@Override
+	public List<Ticket> getTickets() {
+		return entityManager.createQuery("from Ticket order by id", Ticket.class).getResultList();
+	}
+
+	@Override
+	@Transactional
+	public Ticket saveTicket(Ticket ticket) {
+		return entityManager.merge(ticket);
+	}
+
+	@Override
+	public List<Ticket> getTickets(Unit unit) {
+		return entityManager.createQuery("from Ticket where unit=:unit", Ticket.class)
+				.setParameter("unit", unit).getResultList();
+	}
+
+	// Can be fetched from User obj
+//	@Override
+//	public List<Ticket> getTickets(User technician) {
+//		return entityManager.createQuery("from Ticket t join fetch t.technicians u where u=:technician", Ticket.class)
+//				.setParameter("technician", technician).getResultList();
+//	}
+
+
+
+}
