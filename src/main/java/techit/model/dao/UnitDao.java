e1dfc581942c539cc035f07b02f19dc5d86efd9d
Jay Patel <jam12928@gmail.com>
2018-02-23 18:24:10 -0800

    Added DAO for Unit

---
+++ src/main/java/techit/model/dao/UnitDao.java
@@ -0,0 +1,14 @@
+package techit.model.dao;
+
+import java.util.List;
+
+import techit.model.Unit;
+
+public interface UnitDao {
+	
+	Unit getUnit(Long id);
+
+	List<Unit> getUnits();
+
+	Unit saveUnit(Unit unit);
+}
