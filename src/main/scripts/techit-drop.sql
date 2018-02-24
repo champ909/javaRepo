4077ab164f0cbf376a617d63e9d1dfbef682d58e
Jay Patel <jam12928@gmail.com>
2018-02-23 20:32:11 -0800

    updated techit-drop.sql

--- src/main/scripts/techit-drop.sql
+++ src/main/scripts/techit-drop.sql
@@ -1,2 +1,8 @@
-drop table users;
+SET FOREIGN_KEY_CHECKS=0;
 drop table hibernate_sequence;
+drop table users_tickets;
+drop table units;
+drop table tickets;
+drop table updates;
+drop table users;
+SET FOREIGN_KEY_CHECKS=1;
