429d0409ec54deadcf91003a825e3ef48dbba6c1
Jay Patel <jam12928@gmail.com>
2018-02-23 20:30:06 -0800

    Added missing methods in UserDaoImpl

--- src/main/java/techit/model/dao/jpa/UserDaoImpl.java
+++ src/main/java/techit/model/dao/jpa/UserDaoImpl.java
@@ -8,33 +8,43 @@ import javax.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import techit.model.Ticket;
 import techit.model.User;
 import techit.model.dao.UserDao;
 
 @Repository
 public class UserDaoImpl implements UserDao {
 
-    @PersistenceContext
-    private EntityManager entityManager;
+	@PersistenceContext
+	private EntityManager entityManager;
 
-    @Override
-    public User getUser( Long id )
-    {
-        return entityManager.find( User.class, id );
-    }
+	@Override
+	public User getUser(Long id) {
+		return entityManager.find(User.class, id);
+	}
 
-    @Override
-    public List<User> getUsers()
-    {
-        return entityManager.createQuery( "from User order by id", User.class )
-            .getResultList();
-    }
+	@Override
+	public List<User> getUsers() {
+		return entityManager.createQuery("from User order by id", User.class).getResultList();
+	}
 
-    @Override
-    @Transactional
-    public User saveUser( User user )
-    {
-        return entityManager.merge( user );
-    }
+	@Override
+	@Transactional
+	public User saveUser(User user) {
+		return entityManager.merge(user);
+	}
+
+	@Override
+	public User getUser(String username) {
+		return entityManager.createQuery("from User where username =: username", User.class)
+				.setParameter("username", username).getSingleResult();
+	}
+
+	// can be fetched from Ticket obj
+//	@Override
+//	public List<User> getTechnicians(Ticket ticket) {
+//		return entityManager.createQuery("from User u join fetch u.tickets t where t=:ticket", User.class)
+//				.setParameter("ticket", ticket).getResultList();
+//	}
 
 }
